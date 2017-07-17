15f55492502022f2e9e3634257de37888d510cdc
Horea Porutiu <horeaporutiu@gmail.com>
2017-07-17 12:27:16 -0700

    got post request working

--- test.js
+++ test.js
@@ -3,19 +3,22 @@ var request = require('request');
 function main(params) {
   var username = "64a3ecc4-182b-47e9-a659-c580a7b5ca02";
   var password = "AnnGIdp6kCU7";
-  var url = "https://" + username + ":" + password +
-  "@gateway.watsonplatform.net/language-translator/api/v2/translate?source="
-  + params.source + "&target=" + params.target + "&text="+params.text;
+
+  var postURL = "https://" + username + ":" + password +
+  "@gateway.watsonplatform.net/language-translator/api/v2/translate"
 
    return new Promise(function(resolve, reject) {
-       request.get(url, function(error, response, body) {
-           if (error) {
-               reject(error);
-           }
-           else {
-
-               resolve({msg: body});
-           }
-       });
+     request.post({
+       headers: {'content-type' : 'application/json'},
+       url:     postURL,
+       json:    {
+         source: 'en',
+         target: 'es',
+         text: 'something'
+      }
+     },
+     function(error, response, body){
+       resolve({msg: body});
+     });
    });
 }
--- tester2.js
+++ tester2.js
@@ -1,23 +1,45 @@
+// var request = require('request');
+//
+// function main(params) {
+//   var username = "64a3ecc4-182b-47e9-a659-c580a7b5ca02";
+//   var password = "AnnGIdp6kCU7";
+//   var auth = "Basic " + new Buffer(username + ":" + password).toString("base64");
+//   // var url = "https://" + username + ":" + password +
+//   // "@gateway.watsonplatform.net/language-translator/api/v2/translate?source="
+//   // + params.source + "&target=" + params.target + "&text="+params.text;
+//
+//   var options = {
+//   url: 'http://localhost:1234/api/res/xyz',
+//   auth: {
+//     user: username,
+//     password: password
+//   }
+// }
+//
+//    return new Promise(function(resolve, reject) {
+//        request.get(options, function(error, response, body) {
+//            if (error) {
+//                reject(error);
+//            }
+//            else {
+//
+//                resolve({msg: body});
+//            }
+//        });
+//    });
+// }
+
 var request = require('request');
 
 function main(params) {
   var username = "64a3ecc4-182b-47e9-a659-c580a7b5ca02";
   var password = "AnnGIdp6kCU7";
-  var auth = "Basic " + new Buffer(username + ":" + password).toString("base64");
-  // var url = "https://" + username + ":" + password +
-  // "@gateway.watsonplatform.net/language-translator/api/v2/translate?source="
-  // + params.source + "&target=" + params.target + "&text="+params.text;
-
-  var options = {
-  url: 'http://localhost:1234/api/res/xyz',
-  auth: {
-    user: username,
-    password: password
-  }
-}
+  var url = "https://" + username + ":" + password +
+  "@gateway.watsonplatform.net/language-translator/api/v2/translate?source="
+  + params.source + "&target=" + params.target + "&text="+params.text;
 
    return new Promise(function(resolve, reject) {
-       request.get(options, function(error, response, body) {
+       request.get(url, function(error, response, body) {
            if (error) {
                reject(error);
            }
