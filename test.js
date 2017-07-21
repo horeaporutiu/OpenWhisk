e05d57e5faa832dd503e58518a7ad040cb28a7ed
Horea Porutiu <horeaporutiu@gmail.com>
2017-07-21 14:57:15 -0700

    Update test.js

--- test.js
+++ test.js
@@ -1,8 +1,8 @@
 var request = require('request');
 
 function main(params) {
-  var username = "64a3ecc4-182b-47e9-a659-c580a7b5ca02";
-  var password = "AnnGIdp6kCU7";
+  var username = "username";
+  var password = "password";
   var postURL = "https://" + username + ":" + password +
   "@gateway.watsonplatform.net/language-translator/api/v2/translate";
 
