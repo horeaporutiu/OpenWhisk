b761622312e59960790489a39cb83720fc1551cd
Horea Porutiu <horeaporutiu@gmail.com>
2017-07-25 15:14:40 -0700

    OW works for both Xcode/JS

--- test.js
+++ test.js
@@ -1,13 +1,14 @@
 var request = require('request');
 
 function main(params) {
-  var username = "username";
-  var password = "password";
+  var username = "64a3ecc4-182b-47e9-a659-c580a7b5ca02";
+  var password = "AnnGIdp6kCU7";
+
   var postURL = "https://" + username + ":" + password +
   "@gateway.watsonplatform.net/language-translator/api/v2/translate";
 
-  return new Promise(function(resolve, reject) {
-    var req = request.post({
+  var promise = new Promise(function(resolve, reject) {
+    request.post({
       url:     postURL,
       json: {
         'source' : params.source,
@@ -16,7 +17,13 @@ function main(params) {
       }
     },
     function(error, response, body){
-      resolve({body:body.translations[0].translation});
+      resolve ({
+        headers: {
+          'content-type': 'application/json'
+        },
+        body: new Buffer (body.translations[0].translation).toString('base64')
+      });
     });
   });
+  return promise;
 }
