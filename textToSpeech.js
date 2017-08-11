0f44a9a046ac6caa7e6e1457bc4e3952189d7432
Horea Porutiu <horeaporutiu@gmail.com>
2017-08-11 13:29:56 -0700

    new stuff

--- textToSpeech.js
+++ textToSpeech.js
@@ -2,8 +2,8 @@
 var request = require('request');
 
 function main(params) {
-  var username = "03f2282e-db10-4f97-aae5-591ef9c0aa11";
-  var password = "XSoXnuFrkbKg";
+  var username = params.textToSpeechUsername;
+  var password = params.textToSpeechPassword;
   var auth = "Basic " + new Buffer(username + ":" + password).toString("base64");
 
   var textToSpeechUrl = "https://stream.watsonplatform.net/authorization/api/v1/token?url=https://stream.watsonplatform.net/text-to-speech/api";
